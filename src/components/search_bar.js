e430b5ea83f49222cb88d7c1ee4cc6157a19d88f
yuval-rikover <yuval.rikover@kenshoo.com>
2018-02-07 21:45:10 +0200

    mooore

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 class SearchBar extends Component {
-  render() {
-    return <input />;
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''}
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({term: event.target.value})}/>
+            </div>
+        );
+    }
+
 }
 
 export default SearchBar
--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list.js'
 
-const API_KEY = 'AIzaSyB84sPnqzUo7MWRKNQFyLVw5m2m-c3L6Nw'
+const API_KEY = 'AIzaSyB84sPnqzUo7MWRKNQFyLVw5m2m-c3L6Nw';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>  
-  )
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos})
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        )
+    }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'))
+ReactDOM.render(<App/>, document.querySelector('.container'));
